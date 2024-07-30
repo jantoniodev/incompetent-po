0a4163931f6ec221f258648f1434dc1edd5ae7ad
jantoniodev <jose.lopezh95@gmail.com>
2024-07-30 01:44:26 -0400

    add messages to loading

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -1,5 +1,40 @@
+import { useState, useEffect } from 'react'
 import { PropagateLoader } from 'react-spinners'
 
+const messages = [
+    'Trabajando más rápido que tu product owner',
+    'Generando historias de usuario',
+    'Haciendo micromanagement',
+    'Imitando a tu PO: ¿Como vamos?',
+]
+
 export const Loader = () => {
-    return <PropagateLoader color='var(--color-primary)' size={25}/>
+    const [showMessage, setShowMessage] = useState(false)
+    const [selectedMessage, setSelectedMessage] = useState(messages[0])
+
+    useEffect(() => {
+        let interval: NodeJS.Timeout
+
+        const timeout = setTimeout(() => {
+            setShowMessage(true)
+
+            interval = setInterval(() => {
+                const message = messages[Math.floor(Math.random() * messages.length)]
+                setSelectedMessage(message)
+            }, 5000)
+        }, 3000)
+
+
+        return () => {
+            clearTimeout(timeout)
+            clearInterval(interval)
+        }
+    }, [])
+
+    return (
+        <>
+            <PropagateLoader color='var(--color-primary)' size={25}/>
+            {showMessage && <p>{selectedMessage}</p>}
+        </>
+    )
 }
