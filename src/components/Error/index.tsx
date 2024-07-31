193b512e52195d7d925cb0c04887d749e83252e7
jantoniodev <jose.lopezh95@gmail.com>
2024-07-30 23:24:13 -0400

    close error button

--- src/components/Error/index.tsx
+++ src/components/Error/index.tsx
@@ -1,6 +1,9 @@
 'use client'
 
+import { useState } from 'react'
+
 import { IoIosWarning } from "react-icons/io"
+import { IoClose } from "react-icons/io5";
 
 import styles from './error.module.css'
 
@@ -11,9 +14,16 @@ export interface ErrorProps {
 export const Error = ({
     message
 }: ErrorProps) => {
+    const [hideError, setHideError] = useState(false)
+
+    if(hideError) {
+        return null
+    }
+
     return (
         <div className={styles.errorContainer}>
             <IoIosWarning size={25}/> { message }
+            <IoClose size={20} className={styles.closeButton} onClick={() => setHideError(true)}/>
         </div>
     )
 }
